5544a57c241a1ccc4bd47c56df8eb5cf3e129e4b
Hancel.Lin <imlinhanchao@gmail.com>
2022-07-29 20:39:57 +0800

    更新库依赖

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,7 +4,6 @@ import ipc from './ipc'
 import Setting from './setting'
 
 import Vue from 'vue'
-import axios from 'axios'
 import VueWorker from 'vue-worker'
 import FishPi from 'fishpi';
 
@@ -24,7 +23,6 @@ Vue.use(VueWorker)
 Vue.locale = () => { };
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.config.devtools = true;
 
