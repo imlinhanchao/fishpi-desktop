dcb3a3801677aef34e842f44f09322b3da03fcac
Hancel.Lin <imlinhanchao@gmail.com>
2022-09-27 22:58:16 +0800

    添加iframe

---
+++ src/renderer/view/context.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="layout">
+    <iframe></iframe>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'context',
+        components: {
+        },
+        async mounted () {
+            
+        },
+        beforeDestroy () {
+            
+        },
+        data () {
+            return {
+               
+            }    
+        },
+        watch: {
+            
+        },
+        filters: {
+        },
+        computed: {
+           
+        },
+        methods: {
+            
+        }
+    }
+</script>
+<style lang="less" scoped>
+.layout {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    background: var(--main-chatroom-background-color);
+}
+</style>
